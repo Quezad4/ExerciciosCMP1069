da9a600235ada4f519cd13677a0e7a1a97ef6078
Quezada <mateusquezada@live.com>
2025-03-28 19:39:56 -0300

    ex2 finalizado

--- Tarefa1/main.js
+++ Tarefa1/main.js
@@ -2,10 +2,25 @@ let arrayAlunos = []
 
 
 class Aluno{
-    constructor(nome, idade, curso){
+    constructor(nome, idade, curso, nota){
         this.nome = nome;
         this.idade = idade;
         this.curso = curso;
+        this.nota = nota;
+    }
+
+    isAprovado(nota){
+        if(nota>= 7)
+            return true
+        return false
+    }
+    toString(nome, idade, curso, nota){
+        return "Aluno{" +
+        "nome='" + nome + '\'' +
+        ", idade=" + idade +
+        ", curso='" + curso + '\'' +
+        ", nota=" + nota +
+        '}';
     }
 };
 
@@ -17,8 +32,9 @@ botaoCadastrar.addEventListener("click", () => {
     let nome = document.getElementById("nome").value;
     let idade = document.getElementById("idade").value;
     let curso  = document.getElementById("curso").value;
+    let nota = document.getElementById("nota").value;
 
-    newAluno = new Aluno(nome, idade, curso)
+    newAluno = new Aluno(nome, idade, curso, nota)
 
     arrayAlunos.push(newAluno)
     atualizarTabela()
@@ -28,13 +44,47 @@ botaoCadastrar.addEventListener("click", () => {
 
 function atualizarTabela(){
     let tabela = document.getElementById("tabela")
-    console.log(tabela)
+    tabela.innerHTML = `<tr>
+                            <th>Nome</th>
+                            <th>Idade</th>
+                            <th>Curso</th>
+                            <th>Nota Final</th>
+                        </tr>`
+    indice = 0
     arrayAlunos.forEach(aluno => {
         let linha = tabela.insertRow()
         linha.insertCell(0).textContent = aluno.nome;
         linha.insertCell(1).textContent = aluno.idade;
         linha.insertCell(2).textContent = aluno.curso;
+        linha.insertCell(3).textContent = aluno.nota;
+        linha.insertCell(4).innerHTML = `<button onclick = "botaoEditar(${indice})">Editar</button>`
+        linha.insertCell(5).innerHTML = `<button onclick = "botaoExcluir(${indice})">Excluir</button>`
+        indice++;
     });
+
+}
+
+function botaoEditar(indice){
+    console.log("AQUI", indice)
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let curso  = document.getElementById("curso").value;
+    let nota = document.getElementById("nota").value;
+
+    arrayAlunos[indice].nome = nome
+    arrayAlunos[indice].idade = idade
+    arrayAlunos[indice].curso = curso
+    arrayAlunos[indice].nota = nota
+    atualizarTabela()
+    limparInputs()
+
+
+}
+
+function botaoExcluir(indice){
+    arrayAlunos.splice(indice,1)
+    atualizarTabela()
+    limparInputs()
 }
 
 
@@ -42,10 +92,12 @@ function limparInputs(){
     let nome = document.getElementById("nome");
     let idade = document.getElementById("idade");
     let curso  = document.getElementById("curso");
+    let nota  = document.getElementById("nota");
 
     nome.value = "";
     idade.value = "";
     curso.value = "";
+    nota.value = ""; 
 }
 
 
